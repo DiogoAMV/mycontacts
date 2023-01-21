1d0379ca8c4a1901207b5f86b69eac2168866d44
DiogoAMV <diogo.amv19@gmail.com>
2023-01-21 19:30:58 -0300

    :sparkles: feat: primeira rota da API criada

---
+++ server/src/mocks/users.js
@@ -0,0 +1,18 @@
+module.exports = [
+    {
+        id: 1,
+        name: 'Diogo',
+    },
+    {
+        id: 2,
+        name: 'Lucas',
+    },
+    {
+        id: 3,
+        name: 'André',
+    },
+    {
+        id: 4,
+        name: 'Antônio',
+    },
+]
