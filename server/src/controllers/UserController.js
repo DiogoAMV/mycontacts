6004b277c1fd7968763ac608ad33520792c78cbc
DiogoAMV <diogo.amv19@gmail.com>
2023-01-21 20:53:09 -0300

    :sparkles: feat: helper bodyParser criado

--- server/src/controllers/UserController.js
+++ server/src/controllers/UserController.js
@@ -25,22 +25,15 @@ module.exports = {
   },
 
   createUser(request, response) {
-    let body = "";
-    request.on("data", (chunk) => {
-      body += chunk;
-    });
+    const { body } = request;
+    const lastUserId = users[users.length - 1].id;
+    const newUser = {
+      id: lastUserId + 1,
+      name: body.name,
+    };
 
-    request.on("end", () => {
-      body = JSON.parse(body);
-      const lastUserId = users[users.length - 1].id;
-      const newUser = {
-        id: lastUserId + 1,
-        name: body.name,
-      };
+    users.push(newUser);
 
-      users.push(newUser);
-
-      response.send(200, newUser);
-    });
+    response.send(200, newUser);
   },
 };
